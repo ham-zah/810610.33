232e45bf6a20a1fae407bbeb008636e620def272
ham-zah <90850092+ham-zah@users.noreply.github.com>
2021-09-16 21:53:36 +0700

    Create single0.js

---
+++ 75/single0.js
@@ -0,0 +1,3 @@
+
+/* Double Click */
+$('i[rel="pre"]').replaceWith(function(){return $("<pre><code>"+$(this).html()+"</code></pre>")});for(var pres=document.querySelectorAll("pre,code,kbd,blockquote,td"),i=0;i<pres.length;i++)pres[i].addEventListener("dblclick",function(){var e=getSelection(),t=document.createRange();t.selectNodeContents(this),e.removeAllRanges(),e.addRange(t)},!1);
