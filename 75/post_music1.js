7302a4647f02d8428861090d527e26d1c47d52a6
ham-zah <90850092+ham-zah@users.noreply.github.com>
2021-09-17 10:21:19 +0700

    Update and rename post_music0.js to post_music1.js

--- 75/post_music1.js
+++ 75/post_music1.js
@@ -9,7 +9,7 @@ $(".sumberlirik").replaceWith('<div id="msource"><div class="msource_text">Sumbe
 $(".id_yt").each(function(){$(this).replaceWith('<div class="yt_class"><div class="yt_text">Dengarkan di YouTube<a href="https://youtu.be/'+$(this).data("id")+'" rel="nofollow noopener" target="_blank"><span id="yt_link"/></a></div><div class="yt_box"><iframe loading="lazy" src="https://www.youtube.com/embed/'+$(this).data("id")+'" frameborder="0" allowfullscreen></iframe></div></div><div class="dengarkan"><div class="listen_text">Dengarkan di &#10095;</div></div>')});
 
 var lbox1 = '<div class="listen_box"><a href="https://';
-var lboxA = '" rel="nofollow noopener" target="_blank"><img '+lazyM+' data-src="https://cdn.statically.io/img/hamdaniazzah.github.io/icon/';
+var lboxA = '" rel="nofollow noopener" target="_blank"><img '+lazyM+' data-src="https://hamdaniazzah.github.io/icon/';
 var lboxS = lboxA + 'spotify40.svg"/>Spotify</a></div>';
 var lboxJ = lboxA + 'joox40.svg"/>JOOX</a></div>';
 var lboxD = lboxA + 'deezer40.svg"/>Deezer</a></div>';
